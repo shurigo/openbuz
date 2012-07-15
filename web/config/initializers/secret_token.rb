f8eb98fda7466c7f42a7e5a0026f08b0a74742a2
shurigo <igor.shurshin@gmail.com>
2012-07-16 01:20:08 +0400

    Added AWS

--- web/config/initializers/secret_token.rb
+++ web/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Web::Application.config.secret_token = '9755694faf779bab478eda99de99d8e425c66b73825b8c38ba24320c6df84c298e44a90661421d6c558993b1d6bb4bfe2c1ac16b82096b60394541fae8d0c93a'
+Web::Application.config.secret_token = '98daf5488c77571ab39b49520a32527e81e3c2e242615d8f80c19463bded48a4877750eebeb2732b5c265c0ef5fccb5c35cd82451fbc81f43c646616f9a54929'
--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,4 +1,6 @@
 Web::Application.routes.draw do
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
