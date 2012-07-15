c711f4e96a517a90d5c92dbebc6d8f68a69db01c
shurigo <igor.shurshin@gmail.com>
2012-07-16 01:38:23 +0400

    Secret token removed

--- web/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Web::Application.config.secret_token = '98daf5488c77571ab39b49520a32527e81e3c2e242615d8f80c19463bded48a4877750eebeb2732b5c265c0ef5fccb5c35cd82451fbc81f43c646616f9a54929'
