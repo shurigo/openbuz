5c6419c99f05cf3191d12b98a1aa333fed1b5238
shurigo <igor.shurshin@gmail.com>
2012-10-11 00:56:43 +0400

    email downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
             length: { minimum: 6 }
   validates :password_confirmation, :presence => true
 
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
   before_save :create_remember_token
 
   private
